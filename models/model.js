51051d59dcebb747939c26f1a47bdb21bd659a97
Yash Kalwani <87498970+YeshH007@users.noreply.github.com>
2024-08-12 19:35:58 +0530

    Delete models directory

--- models/model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const workspaceschema=mongoose.Schema({
-    workspacename:{type:String},
-    workspacelanguage:{type:String},
-    workspaceid:{type:String},
-    workspacecode:{type:String}
-})
-let workspacemodel=mongoose.model('workspace',workspaceschema,'workspace')
-export {workspacemodel}
